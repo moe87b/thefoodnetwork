1d2a95b78fe7be83751f3a00400e644a648ea7eb
MohamedBdeir <mohamad.bdeir@isae.edu.lb>
2020-07-08 19:47:31 +0300

    Updates

---
+++ precache-manifest.6bc8336b46e959aaba5e6b49657b5d07.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73298626c41615a854ab4199f764e4fc",
+    "url": "/thefoodnetwork/index.html"
+  },
+  {
+    "revision": "5fcd8038cf69e3d91a1b",
+    "url": "/thefoodnetwork/static/js/2.33ce9ad2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/thefoodnetwork/static/js/2.33ce9ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e1e7792b398e512270b",
+    "url": "/thefoodnetwork/static/js/main.f4e81d11.chunk.js"
+  },
+  {
+    "revision": "92d047e18873cb5a30b0",
+    "url": "/thefoodnetwork/static/js/runtime-main.d6e34668.js"
+  }
+]);
